1ae5962f8524bae7265f812edd8ec10d5cb0c426
isherax <sparksflowing@gmail.com>
2023-06-17 16:01:45 -0400

    enable local stable diffusion instance

--- dream_chaser.py
+++ dream_chaser.py
@@ -1,5 +1,18 @@
-from langchain import OPENAI, LLMCHain, PromptTemplate
-import config
-import os
+from diffusers import DiffusionPipeline
+import torch
 
-os.environ['OPENAI_API_KEY'] = config.OPENAI_KEY
+
+class DreamChaserAPI:
+    def __init__(self, model_id):
+        self.pipeline = DiffusionPipeline.from_pretrained(model_id)
+        
+        # move generator to GPU if possible
+        if torch.cuda.device_count() > 0:
+            self.pipeline.to('cuda')
+
+
+if __name__ == '__main__':
+    pretrained_model = './stable-diffusion-v1-5'
+    api = DreamChaserAPI(pretrained_model)
+    image = api.pipeline('An image of a squirrel in Picasso style').images[0]
+    image.save('sample.png')
