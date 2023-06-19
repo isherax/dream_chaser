b8f68901ae9ead3b5e44035a0413d93c814996f9
isherax <sparksflowing@gmail.com>
2023-06-19 14:59:04 -0400

    enable run from command line

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Dream Chaser Backend Server
 
 A backend server for multilevel AI imaging.
+
+## Setup
+
+1. Download pretrained stable diffusion model from huggingface in subdirectory: `git clone https://huggingface.co/runwayml/stable-diffusion-v1-5`
--- dream_chaser.py
+++ dream_chaser.py
@@ -1,5 +1,8 @@
 from diffusers import DiffusionPipeline
+import argparse
 import torch
+import uuid
+import warnings
 
 
 class DreamChaserAPI:
@@ -9,10 +12,18 @@ class DreamChaserAPI:
         # move generator to GPU if possible
         if torch.cuda.device_count() > 0:
             self.pipeline.to('cuda')
+        else:
+            warnings.warn('GPU not found, image generation will be substantially slower.', RuntimeWarning)
 
 
 if __name__ == '__main__':
     pretrained_model = './stable-diffusion-v1-5'
     api = DreamChaserAPI(pretrained_model)
-    image = api.pipeline('An image of a squirrel in Picasso style').images[0]
-    image.save('sample.png')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--p', '--prompt')
+    args = parser.parse_args()
+    images = api.pipeline(args.p).images
+    
+    for i in range(len(images)):
+        temp_file = uuid.uuid4().hex
+        images[i].save(f'samples/{temp_file}.png')
