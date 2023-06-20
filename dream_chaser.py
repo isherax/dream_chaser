9a2884a09dfe7d122c97065509f07bd99afc00a8
isherax <sparksflowing@gmail.com>
2023-06-20 12:34:30 -0400

    switching prompt improvement models

--- dream_chaser.py
+++ dream_chaser.py
@@ -1,7 +1,7 @@
 from diffusers import DiffusionPipeline
-from transformers import T5Tokenizer, T5ForConditionalGeneration
 import argparse
 import torch
+import transformers
 import uuid
 import warnings
 
@@ -9,36 +9,23 @@ import warnings
 class DreamChaserAPI:
     def __init__(self, image_model_id, text_model_id):
         self.image_pipeline = DiffusionPipeline.from_pretrained(image_model_id)
-        self.tokenizer = T5Tokenizer.from_pretrained(text_model_id)
+        self.text_pipeline = transformers.pipeline('text-generation', model=text_model_id)
         
         if torch.cuda.device_count() > 0:
-            self.has_gpu = True
             self.image_pipeline.to('cuda')
-            self.text_model = T5ForConditionalGeneration.from_pretrained(text_model_id)
         else:
             warnings.warn('GPU not found, image generation will be substantially slower.', RuntimeWarning)
-            self.has_gpu = False
-            self.text_model = T5ForConditionalGeneration.from_pretrained(text_model_id, device_map='auto')
-            
+
             
     def improve_prompt(self, text):
-        instructions = 'An ideal prompt for a text to image model should be concise, clear, and describe the artistic style of the image. Expand or summarize the following prompt as necessary to retain the original meaning while optimizing for good image generation'
-        input_text = f'{instructions}: {text}'
+        sequences = self.text_pipeline(text, max_length=77)
         
-        if self.has_gpu:
-            input_ids = self.tokenizer(input_text, return_tensors='pt').input_ids.to('cuda')
-        else:
-            input_ids = self.tokenizer(input_text, return_tensors='pt').input_ids
-            
-        output_tokens = self.text_model.generate(input_ids)
-        output_text = self.tokenizer.decode(output_tokens[0])
-        
-        return output_text
+        return sequences[0]['generated_text']
 
 
 if __name__ == '__main__':
     image_model = './openjourney'
-    text_model = './flan-t5-base'
+    text_model = './gpt2-650k-stable-diffusion-prompt-generator'
     api = DreamChaserAPI(image_model, text_model)
     parser = argparse.ArgumentParser()
     parser.add_argument('--p', '--prompt', required=True)
