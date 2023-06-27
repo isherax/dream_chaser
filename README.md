48f2beffd835e5655c6201d07527c913608086c1
isherax <sparksflowing@gmail.com>
2023-06-27 13:34:38 -0400

    prompt enhancer with similarity ranking

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ A backend server for multilevel AI imaging.
 ## Setup
 
 1. Download pretrained text to image model from huggingface in subdirectory: `git clone https://huggingface.co/prompthero/openjourney-v4`
-2. Download pretrained text generation model from huggingface in subdirectory: `https://huggingface.co/Ar4ikov/gpt2-650k-stable-diffusion-prompt-generator`
+2. Download pretrained prompt generation model from huggingface in subdirectory: `git clone https://huggingface.co/Ar4ikov/gpt2-650k-stable-diffusion-prompt-generator`
+3. Download pretrained sentence similarity model from huggingface in subdirectory: `git clone https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2`
--- start_api.py
+++ start_api.py
@@ -1,6 +1,7 @@
 from diffusers import StableDiffusionPipeline
 from flask import Flask, send_file
 from flask_restful import Resource, Api, reqparse
+from sentence_transformers import SentenceTransformer, util
 import io
 import torch
 import transformers
@@ -13,23 +14,37 @@ class DreamChaserAPI(Resource):
         
             
     @classmethod
-    def setup_models(self, image_model_id, text_model_id):
+    def setup_models(self, image_model_id, prompt_model_id, similarity_model_id):
         self.image_pipeline = StableDiffusionPipeline.from_pretrained(image_model_id) 
-        self.text_pipeline = transformers.pipeline('text-generation', model=text_model_id)
+        self.prompt_pipeline = transformers.pipeline('text-generation', model=prompt_model_id)
+        self.similarity_model = SentenceTransformer(similarity_model_id)
         
         if torch.cuda.device_count() > 0:
             self.image_pipeline.to('cuda')
+            self.prompt_pipeline.to('cuda')
         else:
             warnings.warn('GPU not found, image generation will be substantially slower.', RuntimeWarning)
             
         return self
     
     
-    def enhance_prompt(self, input_text):
-        text = self.text_pipeline(input_text, max_length=80)[0]['generated_text']
-        text = ' '.join(text.split(' ')[:-1 or None])
+    def enhance_prompt(self, input_text, num_gens=5):
+        input_embeddings = self.similarity_model.encode(input_text, convert_to_tensor=True)
+        new_prompt = input_text
+        max_similarity = 0
         
-        return text
+        for i in range(num_gens):
+            text = self.prompt_pipeline(input_text, max_length=77, )[0]['generated_text']
+            text = ' '.join(text.split(' ')[:-1 or None])
+            new_embeddings = self.similarity_model.encode(text, convert_to_tensor=True)
+            cos_score = util.cos_sim(input_embeddings, new_embeddings).numpy()[0][0]
+            
+            if cos_score > max_similarity:
+                max_similarity = cos_score
+                new_prompt = text
+            
+        
+        return new_prompt
             
             
     def post(self):
@@ -52,9 +67,10 @@ class DreamChaserAPI(Resource):
     
 
 if __name__ == '__main__':
-    image_model = './openjourney-v4'
-    text_model = './gpt2-650k-stable-diffusion-prompt-generator'
-    dca = DreamChaserAPI.setup_models(image_model, text_model)
+    image_model_folder = './openjourney-v4'
+    text_model_folder = './gpt2-650k-stable-diffusion-prompt-generator'
+    similarity_model_folder = './all-MiniLM-L6-v2'
+    dca = DreamChaserAPI.setup_models(image_model_folder, text_model_folder, similarity_model_folder)
     app = Flask(__name__)
     api = Api(app)
     api.add_resource(dca, '/image')
